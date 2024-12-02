cf6294fced3e97ae6e5f12a0fc1003f4054c35de
cutetoast <muhdmokhtar06@gmail.com>
2024-12-02 11:07:29 +0800

    update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -18,7 +18,7 @@ const Dashboard = () => {
   const auth = getAuth();
 
   useEffect(() => {
-    const ws = new WebSocket("wss://http://0.0.0.0:8000 /ws");
+    const ws = new WebSocket("wss://http://0.0.0.0:8000");
 
     ws.onopen = () => console.log("WebSocket connection established.");
 
