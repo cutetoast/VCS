d3593c7797586a4421901a03ed32bf0c76a41183
cutetoast <muhdmokhtar06@gmail.com>
2024-12-26 04:17:35 +0800

    update

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Enable CORS for the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vcs-gray.vercel.app/"],
+    allow_origins=["https://vcs-gray.vercel.app"],
  #  allow_origins=["http://localhost:5174"],https://vcs-gray.vercel.app/
     allow_credentials=True,
     allow_methods=["*"],
